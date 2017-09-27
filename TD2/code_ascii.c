e7041cba1baba14ad8bdb7477be5c6bd10387360
Ablo224 <baldeabdoulaye06@gmail.com>
2017-09-28 01:54:12 +0200

    code ascii

---
+++ TD2/code_ascii.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	char code;
+	printf("Entré le caractère: ");
+	scanf("%c",&code);
+	printf("\n%d code ASCII de %c\n",code,code);
+	
+	return 0;
+}
